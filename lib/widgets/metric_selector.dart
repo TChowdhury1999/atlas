5a10e7ebcd46daf7c277ede8e0270d9f9d68e0ec
Tanjim Chowdhury <tanjim@live.co.uk>
2026-03-14 15:05:33 +0000

    metric selector visual changes

--- lib/widgets/metric_selector.dart
+++ lib/widgets/metric_selector.dart
@@ -10,15 +10,38 @@ class MetricSelector extends StatefulWidget {
 
 class _MetricSelectorState extends State<MetricSelector> {
 
-  List<String> selectedMetrics = ["Weight", "Waist", "Bench", "Lat Pull"];
+  List<String> selectedMetrics = ["Weight", "Waist", "Bench", "Lat Pull", "Other"];
+  Set<String> activeMetrics = {'Weight'};
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16.0),
+      padding: const EdgeInsets.all(8.0),
       child: Row(
-        children:  selectedMetrics.map((metric) => FilterChip(label: Text(metric), onSelected: (val) {})).toList(),
-        )
+        children:  selectedMetrics.map((metric) => Expanded(
+          child: FilterChip(
+            label: Text(
+              metric,
+              style: TextStyle(fontSize: 12),
+              ),
+            selected: activeMetrics.contains(metric),
+            onSelected: (val) {
+              setState(() {
+                val ? activeMetrics.add(metric) : activeMetrics.remove(metric);
+              });
+            },
+            labelStyle: TextStyle(color: AppTheme.textPrimary),
+            selectedColor: AppTheme.surface.withAlpha(204),
+            backgroundColor: AppTheme.surface,
+            labelPadding: EdgeInsets.zero,
+            padding: EdgeInsets.symmetric(horizontal: 6),
+            showCheckmark: false,
+            side: activeMetrics.contains(metric)
+                ? BorderSide(color: AppTheme.accent)
+                : BorderSide.none,
+            ),
+        )).toList(),
+      )
     );
   }
 }
